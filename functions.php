a0da0cddd0996241b5bd6dd330461925dfb5ef6e
nathanmerry9713@gmail.com <nathanmerry9713@gmail.com>
2023-10-29 16:35:39 +0545

    update

--- functions.php
+++ functions.php
@@ -15,24 +15,6 @@ function dump(...$variables)
 
 add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
 
-function create_custom_post_type()
-{
-    register_post_type(
-        'camps',
-        array(
-            'labels' => array(
-                'name' => __('Camps'),
-                'singular_name' => __('Camp'),
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'camps'),
-        )
-    );
-}
-
-add_action('init', 'create_custom_post_type');
-
 function hide_editor()
 {
     // Check if you are on the post editing screen for the desired post type(s).
@@ -62,11 +44,55 @@ function validateUKPostcode($postcode)
     }
 }
 
-$errors = [];
+function slugify($text)
+{
+    $text = strip_tags($text);
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+    setlocale(LC_ALL, 'en_US.utf8');
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    $text = preg_replace('~[^-\w]+~', '', $text);
+    $text = trim($text, '-');
+    $text = preg_replace('~-+~', '-', $text);
+    $text = strtolower($text);
+    if (empty($text)) {
+        return 'n-a';
+    }
+    return $text;
+}
 
+function getPostcodeTransientKey($postcode)
+{
+    return slugify('location ' . $postcode);
+}
+
+function fetchPostcodeLocationData(mixed $postcode): ?array
+{
+    $url = "https://api.postcodes.io/postcodes/$postcode";
+
+    $response = wp_safe_remote_get($url);
+
+    if (is_wp_error($response) || 200 !== wp_remote_retrieve_response_code($response)) return null;
+
+    $data = json_decode(wp_remote_retrieve_body($response), true);
+
+    if (!isset($data['result'])) return null;
+
+    return $data['result'];
+}
+
+/**
+ * Camps class for managing camp data.
+ */
 class Camps
 {
+    /**
+     * @var array An array to store camp data.
+     */
     public $campData = [];
+
+    /**
+     * @var array An array to store validation errors.
+     */
     public $errors = [];
 
     /**
@@ -75,14 +101,14 @@ class Camps
      * @param string|null $givenPostcode The postcode for filtering or null for no filtering.
      * @param float|null $maxDistance The maximum distance for filtering or null for no filtering.
      */
-    public function setCampData($givenPostcode, $maxDistance)
+    public function setCampData(?string $givenPostcode, ?float $maxDistance): void
     {
         $errors = $this->validate($givenPostcode);
 
         if (count($errors)) {
             $this->errors = $errors;
             return;
-        };
+        }
 
         $campData = $this->fetchPosts();
 
@@ -91,30 +117,48 @@ class Camps
             return;
         }
 
-        $givenCoordinates = $this->fetchPostcodeCoordinates($givenPostcode);
+        $givenCoordinates = $this->fetchPostcodeLocationData($givenPostcode);
 
         if (!$givenCoordinates) {
             $this->errors = ['postcode' => 'Please provide a valid postcode'];
             return;
         }
 
-        foreach ($campData as $key => $item) {
-            $coordinates = $this->fetchPostcodeCoordinates($item['postcode']);
-            $campData[$key]['distance'] = $this->calculateHaversineDistance($givenCoordinates, $coordinates);
-        }
-
-        $filtered = $maxDistance ? array_filter($campData, function ($item) use ($maxDistance) {
-            return $item['distance'] <= $maxDistance;
-        }) : $campData;
-
-        usort($filtered, function ($a, $b) {
-            return $a['distance'] - $b['distance'];
-        });
-
-        $this->campData = $filtered;
+        $this->campData = $this->transform($campData, $givenCoordinates, $maxDistance);
     }
 
-    public function validate($postcode)
+    private function transform(array $campData, $givenCoordinates, $maxDistance): array
+    {
+        $transformed = [];
+
+        foreach ($campData as $item) {
+            if (!($item['latitude'] ?? null) || !($item['longitude'] ?? null)) continue;
+
+            $coordinates = [
+                'latitude' => $item['latitude'],
+                'longitude' => $item['longitude'],
+            ];
+
+            $item['distance'] = $this->calculateHaversineDistance($givenCoordinates, $coordinates);
+
+            $transformed[] = $item;
+        }
+
+        $filtered = $maxDistance ? array_filter($transformed, fn ($item) => $item['distance'] <= $maxDistance) : $campData;
+
+        usort($filtered, fn ($a, $b) => $a['distance'] <=> $b['distance']);
+
+        return $filtered;
+    }
+
+    /**
+     * Validate a UK postcode.
+     *
+     * @param string $postcode The postcode to validate.
+     *
+     * @return array An array of validation errors.
+     */
+    private function validate(string|null $postcode): array
     {
         $errors = [];
 
@@ -125,12 +169,17 @@ class Camps
         return $errors;
     }
 
-    public function fetchPosts()
+    /**
+     * Fetch camp data from a data source.
+     *
+     * @return array An array of camp data.
+     */
+    private function fetchPosts(): array
     {
-        $args = array(
-            'post_type' => 'camps', // Replace 'camps' with your custom post type name
-            'posts_per_page' => -1, // Retrieve all posts of the custom post type
-        );
+        $args = [
+            'post_type' => 'camps',
+            'posts_per_page' => -1,
+        ];
 
         $query = new WP_Query($args);
 
@@ -140,7 +189,9 @@ class Camps
             while ($query->have_posts()) {
                 $query->the_post();
                 $fields = get_fields();
-                $campData[] = $fields;
+                if ($fields) {
+                    $campData[] = $fields;
+                }
             }
             wp_reset_postdata();
         }
@@ -151,15 +202,12 @@ class Camps
     /**
      * Calculate the great-circle distance between two points on the Earth's surface using the Haversine formula.
      *
-     * This function computes the distance between two geographic coordinates (latitude and longitude)
-     * on the Earth's surface. The result is the shortest distance (in kilometers) over the Earth's
-     * surface, following the curvature of the Earth.
-     *
      * @param array $coord1 An associative array containing latitude and longitude of the first point.
      * @param array $coord2 An associative array containing latitude and longitude of the second point.
+     *
      * @return float The calculated distance in kilometers.
      */
-    function calculateHaversineDistance($coord1, $coord2)
+    private function calculateHaversineDistance(array $coord1, array $coord2): float
     {
         $earthRadius = 6371; // Earth's radius in kilometers (mean value)
 
@@ -179,25 +227,154 @@ class Camps
         return $distance;
     }
 
-    function fetchPostcodeCoordinates($postcode)
+    /**
+     * Fetch coordinates for a given postcode.
+     *
+     * @param string $postcode The postcode to fetch coordinates for.
+     *
+     * @return array|null An associative array containing latitude and longitude, or null if coordinates cannot be retrieved.
+     */
+    private function fetchPostcodeLocationData(string $postcode): ?array
     {
-        // https://api.postcodes.io/postcodes/rm126ah
-        $apiKey = '3lM3xPOQJNCssr6Jwn5GLL9ePHd96I3v'; // Replace with your Ordnance Survey API key
-        $url = "https://api.postcodes.io/postcodes/$postcode";
+        $locationData = get_transient(getPostcodeTransientKey($postcode));
 
-        $response = wp_safe_remote_get($url);
+        if (!$locationData) {
+            $locationData = fetchPostcodeLocationData($postcode);
 
-        if (is_wp_error($response) || 200 !== wp_remote_retrieve_response_code($response)) return null;
+            if (!$locationData) return null;
 
-        $data = json_decode(wp_remote_retrieve_body($response), true);
+            set_transient(getPostcodeTransientKey($postcode), $locationData);
+        }
 
-        if (!isset($data['result'])) return null;
-
-        $coordinates = [
-            'latitude' => $data['result']['latitude'],
-            'longitude' => $data['result']['longitude'],
+        return [
+            'latitude' => $locationData['latitude'],
+            'longitude' => $locationData['longitude'],
         ];
-
-        return $coordinates;
     }
 }
+
+function handleImportPages($request)
+{
+    // Get the data from the request
+    $campData = $request->get_json_params();
+
+    if (empty($campData)) {
+        return new WP_REST_Response(array('error' => 'Invalid data'), 400);
+    }
+
+    $transformed = [];
+
+    foreach ($campData as $item) {
+        $openingTimes = explode('-', $item['Hours']);
+        $extendedOpeningTimes = explode('-', $item['Extended Hours']);
+        $ages = explode('-', explode('year', $item['Ages?'])[0]);
+
+        $transformedItem = [
+            'name' => $item['Camp Name'] . ' - ' . explode(',', $item['Location'])[0],
+            'location' => $item['Location'],
+            'postcode' => $item['Postcode'],
+            'opening_time' => $openingTimes[0],
+            'closing_time' => $openingTimes[1],
+            'extended_opening_time' => $extendedOpeningTimes[0],
+            'extended_closing_time' => $extendedOpeningTimes[1],
+            'opening_months' => [],
+            'min_age' => trim($ages[0]),
+            'max_age' => trim($ages[1]),
+            'childcare_vouchers_tax_free' => $item['Childcare vouchers '][' Tax free?'] === 'Yes' ? true : false,
+            'url' => $item['URL'],
+            'logo' => attachment_url_to_postid($item['Logo']),
+        ];
+
+        $openingMonthsKeys = ['Feb', 'Easter', 'May', 'Summer', 'Oct', 'Christmas'];
+
+        foreach ($openingMonthsKeys as $month) {
+            if ($item[$month] === 'Yes') {
+                array_push($transformedItem['opening_months'], $month);
+            }
+        }
+
+        $transformed[] = $transformedItem;
+
+        insertPost($transformedItem);
+    }
+
+    foreach ($transformed as $item) {
+        if (!$item['closing_time']) {
+            print_r($item);
+        }
+    }
+
+    return $transformed;
+}
+
+function insertPost($data)
+{
+    $newPost = array(
+        'post_title' => $data['name'],
+        'post_type' => 'camps',
+        'post_status' => 'publish',
+    );
+
+    $postId = wp_insert_post($newPost);
+
+    foreach ($data as $fieldKey => $fieldValue) {
+        update_field($fieldKey, $fieldValue, $postId);
+    }
+}
+
+// Register a custom REST API route
+function register_custom_import_route()
+{
+    register_rest_route('/v1', '/import', array(
+        'methods' => 'GET', // You can use 'GET' or 'POST' depending on your needs
+        'callback' => 'handleImportPages',
+    ));
+}
+
+add_action('rest_api_init', 'register_custom_import_route');
+
+
+function handleScrapLongLat($request)
+{
+    $query = $request->get_query_params();
+    $overwrite = $query['overwrite'] ?? false;
+
+    $args = [
+        'post_type' => 'camps',
+        'posts_per_page' => -1,
+    ];
+
+    $query = new WP_Query($args);
+
+    $updated = [];
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $postId = get_the_ID();
+            $fields = get_fields($postId);
+
+            if (!$fields['latitude'] || !$fields['longitude'] || $overwrite) {
+                $locationData = fetchPostcodeLocationData($fields['postcode']);
+                $updated[] = $fields;
+
+                update_field('latitude', $locationData['latitude'] ?? null, $postId);
+                update_field('longitude', $locationData['longitude'] ?? null, $postId);
+            }
+        }
+        wp_reset_postdata();
+    }
+
+    return $updated;
+}
+
+// Register a custom REST API route
+function registerScrapeLocationRoute()
+{
+    register_rest_route('/v1', '/scrape-long-lat', array(
+        'methods' => 'POST',
+        'callback' => 'handleScrapLongLat',
+    ));
+}
+
+add_action('rest_api_init', 'registerScrapeLocationRoute');
--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 </head>
 
 <?php
+$hasInput = !!($_GET['postcode'] ?? null) && !!($_GET['distance'] ?? null);
 $camps = new Camps();
 $camps->setCampData($_GET['postcode'] ?? null, $_GET['distance'] ?? null);
 ?>
@@ -32,7 +33,7 @@ $camps->setCampData($_GET['postcode'] ?? null, $_GET['distance'] ?? null);
       <img src="http://localhost/wp-content/uploads/2023/08/logo-new.png" alt="Image Description" class=h-[50px]>
     </div>
     <div class="overflow-hidden bg-center bg-cover bg-base-200" style="background-image: url(http://localhost/wp-content/uploads/2023/08/hero.jpg)">
-      <div class="container pt-20 mx-auto pb-28">
+      <div class="container flex justify-center pt-20 mx-auto pb-28">
         <div class="inline-block text-center text-white border-4 border-white">
           <div class="px-24 pt-10 pb-2">
             <h1 class="text-4xl font-bold font-oswald">SUMMER HOLIDAY CAMP</h1>
@@ -54,74 +55,89 @@ $camps->setCampData($_GET['postcode'] ?? null, $_GET['distance'] ?? null);
 
       <div class="w-1/3">
         <label class="text-white label" for="">Distance (kilometers)</label>
-        <input class="w-full input input-bordered" type="number" min="0" name="distance" placeholder="Distance (KM)" value="<?php echo $_GET['distance'] ?? null ?>">
+        <input class="w-full input input-bordered" type="number" min="0" name="distance" placeholder="Distance (KM)" required value="<?php echo $_GET['distance'] ?? null ?>">
       </div>
 
-      <button class="w-1/3 btn outline-none border-0 text-[#203240] bg-[#fdb900]">Search</button>
+      <button class="w-1/3 btn outline-none border-0 text-[#203240] bg-[#fdb900] hover:!bg-[#ffd04e]">Search</button>
     </form>
-    <section class="py-10 text-left">
-      <div class="container mx-auto">
-        <?php if (count($camps->campData)) : ?>
-          <div class="flex grid grid-cols-3 gap-4 gap-5">
-            <?php foreach ($camps->campData as $camp) : ?>
-              <div class="flex flex-col justify-between w-full overflow-hidden rounded shadow-xl card bg-base-100">
-                <div class="flex-1">
-                  <div class="relative">
-                    <div class="h-[220px] flex items-center bg-gray-100">
-                      <img src="<?php echo $camp['logo'] ?>" class="w-full" alt="Image Description">
-                    </div>
+    <section class="container mx-auto text-left">
+      <?php if (count($camps->campData)) : ?>
+        <div class="py-7">
+          <?php if ($hasInput) : ?>
+            Found <strong><?php echo count($camps->campData) ?> holiday camps</strong> within <?php echo $_GET['distance'] ?> kilometers of <?php echo $_GET['postcode'] ?>
+          <?php else : ?>
+            <span class="font-medium">Showing all holiday camps. Search above to find camps within your area...</span>
+          <?php endif; ?>
+        </div>
+        <div class="flex grid grid-cols-3 gap-6">
+          <?php foreach ($camps->campData as $camp) : ?>
+            <div class="flex flex-col justify-between w-full overflow-hidden rounded shadow-xl card bg-base-100">
+              <div class="flex-1">
+                <div class="relative">
+                  <div class="h-[220px] flex items-center bg-gray-100 overflow-hidden">
+                    <img src="<?php echo $camp['logo'] ?>" class="w-full" alt="Image Description">
+                  </div>
+                  <?php if ($camp['min_age'] ?? null && $camp['max_age']) : ?>
                     <div class="absolute top-0 right-0 p-2 text-xl text-white font-caveatBrush bg-[#2b6fb1]">
                       <div class="text-sm">ages</div>
                       <div><span class="font-bold"><?php echo $camp['min_age'] ?> - <?php echo $camp['max_age'] ?></span> <span class="text-xs">years</span></div>
                     </div>
-                  </div>
-                  <div class="w-full px-5 py-4 text-base-content">
-                    <h2 class="block my-0 mb-1 text-3xl font-semibold font-caveatBrush text-base-content"><?php echo $camp['name'] ?></h2>
-                    <a href="St. Johns School, Stock Rd, Billericay, Essex, CM12 0A" class="block mb-4 font-medium text-opacity-60 text-base-content">
-                      <?php echo $camp['location'] ?>
-                    </a>
-                    <div class="flex flex-col justify-between mb-4">
+                  <?php endif; ?>
+                </div>
+                <div class="w-full px-5 py-4 text-base-content">
+                  <h2 class="block my-0 mb-1 text-3xl font-semibold font-caveatBrush text-base-content"><?php echo $camp['name'] ?></h2>
+                  <a href="St. Johns School, Stock Rd, Billericay, Essex, CM12 0A" class="block mb-4 font-medium text-opacity-60 text-base-content">
+                    <?php echo $camp['location'] ?>
+                  </a>
+                  <div class="flex flex-col justify-between mb-4">
+                    <?php if ($camp['opening_time'] ?? null && $camp['closing_time'] ?? null) : ?>
                       <div class="">
                         <span class="inline-block mr-1 font-semibold ">
                           Hours:
                         </span>
                         <span class=""><?php echo $camp['opening_time'] ?> - <?php echo $camp['closing_time'] ?></span>
                       </div>
+                    <?php endif; ?>
+                    <?php if ($camp['extended_closing_time'] ?? null) : ?>
                       <div class="">
                         <span class="font-semibold">
                           Ext. Hours:
                         </span>
                         <span class=""><?php echo $camp['extended_opening_time'] ?> - <?php echo $camp['extended_closing_time'] ?></span>
                       </div>
-
-                    </div>
+                    <?php endif; ?>
+                  </div>
+                  <?php if ($camp['opening_months'] ?? null) : ?>
                     <div class="mb-4">
                       <span class="inline-block mr-1 font-semibold">
                         <i class="text-primary fa-regular fa-calendar"></i>
-                        <!-- Running during: -->
                       </span>
                       <span class=""><?php echo implode(', ', $camp['opening_months']) ?></span>
                     </div>
-                    <?php if ($camp['childcare_vouchers_tax_free']) : ?>
-                      <div class="mb-4">
-                        <div>
-                          <i class="mr-1 text-primary fa-regular fa-star"></i>
-                          Accepts Childcare Vouchers
-                        </div>
+                  <?php endif; ?>
+                  <?php if ($camp['childcare_vouchers_tax_free']) : ?>
+                    <div class="mb-4">
+                      <div>
+                        <i class="mr-1 text-primary fa-regular fa-star"></i>
+                        Accepts Childcare Vouchers
                       </div>
-                    <?php endif; ?>
-                  </div>
-                </div>
-                <div class="w-full px-5 pb-4">
-                  <a href="<?php echo $camp['url'] ?>" class="w-full no-underline btn bg-[#2b6fb1] text-white">Visit Website <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>
+                  <?php endif; ?>
                 </div>
               </div>
-            <?php endforeach; ?>
-          </div>
-        <?php else : ?>
+              <?php if ($camp['url'] ?? null) : ?>
+                <div class="w-full px-5 pb-4">
+                  <a href="<?php echo $camp['url'] ?>" class="w-full no-underline btn bg-[#2b6fb1] text-white hover:bg-[#63809d]">Visit Website <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+              <?php endif; ?>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      <?php else : ?>
+        <div class="py-6">
           There are no results within that distance from the given postcode
-        <?php endif; ?>
-      </div>
+        </div>
+      <?php endif; ?>
     </section>
   </div>
   <script>
